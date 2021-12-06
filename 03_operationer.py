83e3d9b0516f7176bab9873e04f3d4bc2f4b14ef
Lars <lasg@lasg.dk>
2021-12-06 11:11:16 +0100

    test

--- 03_operationer.py
+++
@@ -1,1 +0,0 @@
-# Pythonprogrammering, 2021-12-06, Lars Sommer, Teknologisk Institut
---
+++ 03_udtryk.py
@@ -0,0 +1,22 @@
+# Pythonprogrammering, 2021-12-06, Lars Sommer, Teknologisk Institut
+
+# Overordnede udtrykstyper/expressions: Assignments, aritmetiske, metodekald, deklarationer
+# Husk at køre Pythons stavekontrol (inkl PEP8 kontrol), som bl.a. fikser min manglende whitespace omkring "=" herunder:
+
+var1 = "hej"
+var2 = "med"
+var3 = "dig"
+
+# Strengkonkatenation:
+print(var1 + var2 + var3)
+
+sammenklistret_var = var1 + var2 + var3
+print(sammenklistret_var)
+
+# Aritmetisk eksempel:
+print("10 + 20")
+print(10 + 20)
+
+# Outputtet af divisioner er kommatal:
+print(type(20 / 3))
+
