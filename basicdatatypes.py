1b4dc245afe6315bfa876f4608ee91c7eda245c8
Admin <Admin@TA-8214-09.kursdom.net>
2021-12-06 10:25:15 +0100

    Initial commit

---
+++ basicdatatypes.py
@@ -0,0 +1,11 @@
+
+#Pythonprogrammering, 2021-12-06, Lars Sommer, Teknologisk Institut
+
+#Basic data types in Python:
+#Tal: Heltal, floating point tal, komplekse tal
+#Strenge: Kan indkapsles i enten "dobbeltquotes" eller 'enkeltquotes'
+#Boolske værdier: True eller False
+
+#Ctrl+Shift+F10 to run
+
+print(10)
